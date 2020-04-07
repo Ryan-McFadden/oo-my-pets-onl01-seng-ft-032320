d86341230037b83a3533577a31e79608aee3e66b
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-07 20:28:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,75 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @dogs = []
+    @cats = []
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.count 
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats 
+    Cats.all.each do |cat|
+      if cat.owner == self
+        @cats << cat
+      end
+    end
+    @cats
+  end
+  
+  def dogs 
+    Dogs.all.each do |dog|
+      if dog.owner == self 
+        @dogs << dog 
+      end
+    end
+    @dogs
+  end
+  
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    new_cat.owner = self
+  end
+  
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    new_dog.owner = self
+  end
+  
+  def walk_dogs
+    
+  end
+  
+  def feed_cats 
+    
+  end
+  
+  def sell_pets
+    
+  end
+  
+  def list_pets
+    "I have #{@dogs.count} dog(s), and #{@cats.count} cat(s)."
+  end
+  
 end
